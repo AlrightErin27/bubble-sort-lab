30f5f06a291e730dbc7f442958542889be9e95fb
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-05 11:57:37 -0700

    added js version

--- bubble-sort.js
+++ bubble-sort.js
@@ -5,11 +5,18 @@ ex4 = [90, -22, -20, 1];
 // console.log(ex3);
 
 const bubbleSort = (arr) => {
+  //get length of arr
   let len = arr.length;
+  //sort through the length w/i (amount of times the code below runs)
   for (let i = 0; i < len; i++) {
+    //interior loop
     for (let j = 0; j < len; j++) {
+      //if the current indexed number is bigger than
+      //the integer to the right then:
       if (arr[j] > arr[j + 1]) {
+        //the result in the current indexed number
         let result = arr[j];
+        //swap
         arr[j] = arr[j + 1];
         arr[j + 1] = result;
       }
